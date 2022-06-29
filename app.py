0c9bbd87b7f7ce8504250f58bfc398fc08146434
chanvitj <chanvitj@yahoo.com>
2022-06-29 22:45:51 +0700

    Add files via upload
    initial

---
+++ app.py
@@ -0,0 +1,13 @@
+import socket
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def Home():
+   hostname=socket.gethostname()
+   ipAddr=socket.gethostbyname(hostname)
+   return "<h1>Hello, "+hostname+", "+ipAddr+"</h1>"
+
+if __name__ == '__main__':
+ app.debug = True
+ app.run(host='0.0.0.0', port=8000)
