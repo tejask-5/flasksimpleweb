2a65cac8665c8f7613d1f0a20995a5c375e4acbd
chanvitj <chanvitj@yahoo.com>
2022-07-02 22:52:47 +0700

    Update app.py
    YYY

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def Home():
    hostname=socket.gethostname()
    ipAddr=socket.gethostbyname(hostname)
-   return "<h1>HelloXXX, "+hostname+", "+ipAddr+"</h1>"
+   return "<h1>HelloXXXYYY, "+hostname+", "+ipAddr+"</h1>"
 
 if __name__ == '__main__':
  app.debug = True
