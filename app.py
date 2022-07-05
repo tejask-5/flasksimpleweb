d0464bae3c882d97368d89ecce2132c037b7e286
chanvitj <chanvitj@yahoo.com>
2022-07-05 08:27:02 +0700

    Update app.py
    add uuid

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def Home():
    ipAddr=socket.gethostbyname(hostname)
    return "<h1>Hello Jew, "+hostname+", "+ipAddr+"</h1>"
 
+@app.route('/e3540078-1fed-4f26-9d80-2060b71c00d0.html')
+def uuidchecking():
+   return ""
+
+@app.route('/forti-uuid.html')
+def uuidDetailCheck():
+   return "<forti-uuid hidden>e3540078-1fed-4f26-9d80-2060b71c00d0</forti-uuid>"
+
 if __name__ == '__main__':
  app.debug = True
  app.run(host='0.0.0.0', port=8000)
