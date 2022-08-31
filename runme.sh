82311757e4b3da6ea77bb56e50bf15d6628bf21a
ibsina <35305255+ibsina@users.noreply.github.com>
2022-08-31 11:03:26 +0800

    Update runme.sh
    change  to seahk

--- runme.sh
+++ runme.sh
@@ -3,4 +3,4 @@
 date >> output.log
 whoami >> output.log
 pwd >> output.log
-FLASK_APP=app.py  FLASK_ENV=development  /home/ubuntu/anaconda3/bin/flask  run  --port 8000  --host=0.0.0.0
+FLASK_APP=app.py  FLASK_ENV=development  /home/seahk/anaconda3/bin/flask  run  --port 8001  --host=0.0.0.0
