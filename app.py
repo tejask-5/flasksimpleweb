1d970fa233f36ba28d2438710a9fe4b3953a23a6
ibsina <35305255+ibsina@users.noreply.github.com>
2022-10-05 10:47:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def Home():
    hostname=socket.gethostname()
    ipAddr=socket.gethostbyname(hostname)
-   return "<h1>Hello awesome Peoples here + scanned . Thanks "+hostname+", "+ipAddr+"</h1>"
+   return "<h1>Hello awesome Peoples here + scanned . Thankyou "+hostname+", "+ipAddr+"</h1>"
 
 @app.route('/8d7ed8ec-203c-4677-adda-a5bcde82b1c4.html')
 def uuidchecking():
